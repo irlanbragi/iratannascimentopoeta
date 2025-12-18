0eb4b2dcdc363c92afc3bf63103a0bdbddd22906
bragiestudios <bragi.estudios@gmail.com>
2025-12-17 21:37:18 -0300

    Update cordeis.js

--- cordeis.js
+++ cordeis.js
@@ -107,8 +107,261 @@ O amor não escolhe um lado,
 Não tem certo, nem errado, 
 Não é feio, nem pecado,
 É tão fácil e complicado.`
+  },
+
+  {
+    title: "ELAS",
+    date: "01/05/2024",
+    text: `Na força, na raça 
+Tristeza, alegria
+Muito mais que mulher
+Muito mais que Maria
+
+Em casa, na rua
+Trabalho ou folga
+Muito mais que mulher
+Muito mais do que Olga 
+
+Na quadra, no campo
+No ringue, na pista
+Muito mais que mulher
+Muito mais do que Rita
+
+Escritório, hospital 
+Indústria, quartel
+Muito mais que mulher 
+Muito mais que Raquel 
+
+No shopping, na feira
+Plebe ou elite
+Muito mais que mulher
+Muito mais que Judite
+
+Médica, empresária
+Gari ou juiza
+Muito mais que mulher 
+Muito mais que Luiza 
+
+Veterana, mocinha
+Mulher ou menina
+Muito mais que mulher 
+Muito mais que Regina 
+
+Senado, governo
+Câmara, assembleia 
+Muito mais que mulher 
+Muito mais do que Léia 
+
+Antiga, moderna
+Ou contemporânea 
+Muito mais que mulher 
+Muito mais do que Vânia 
+
+Negra, loira
+Morena ou branca
+Muito mais que mulher 
+Muito mais que Bianca
+
+Na dor, na luta
+Paixão, gravidez 
+Muito mais que mulher 
+Muito mais que Inês 
+
+Mãe, filha
+Pai, família 
+Muito mais que mulher
+Muito mais que Emília 
+
+Na arte, na vida
+Cantora, atriz 
+Muito mais que mulher
+Mais que Beatriz 
+
+Serena, guerreira
+Sinônimo de vida
+Muito mais que mulher 
+Mais que Margarida 
+
+Da submissão 
+À independência 
+Muito mais que mulher
+Muito mais que Hortênsia 
+
+O seu tempo chegou
+Seja você o que quer 
+Muito mais que uma fêmea 
+Muito mais que mulher`
+  },
+
+  {
+    title: "Vivendo com a escravidão",
+    date: "13/05/2018",
+    text: `Em mil, oitocentos e oitenta e oito
+Foi anunciada a abolição 
+Não era pra existir 
+Nenhuma segregação 
+E todos comemoravam
+O fim da escravidão 
+
+No entanto até hoje
+Existe a separação 
+Não se vive em liberdade
+Onde existe opressão 
+E ainda se comemora
+O fim da escravidão 
+
+Crianças brancas ou negras
+Que sem ter outra opção 
+Fazem da rua o seu lar
+Sua cama, um papelão 
+E outros comemorando
+O fim da escravidão 
+
+Nas filas dos hospitais
+Negligência e omissão 
+Pacientes sem leitos
+Faltando medicação 
+E lá fora comemoram 
+O fim da escravidão 
+
+Adolescentes no tráfico 
+Perdem a razão e a noção 
+Perdem suas vidas mais cedo 
+E nesta guerra então 
+Ainda se comemora
+O fim da escravidão 
+
+A violência só cresce
+Com dor e sem compaixão 
+Não se respeita idade
+Nem crença e religião 
+E todos comemorando 
+O fim da escravidão 
+
+Nos subúrbios e nas favelas
+A desigualdade então 
+Toma conta desta gente 
+Causando indignação 
+E outros comemorando
+O fim da escravidão 
+
+São tantos impostos pagos
+Mal dar dar pra comprar o pão 
+Se não existe o chicote
+Sobra humilhação 
+E o senhorzinho comemora
+O fim da escravidão 
+
+E o povo sem cultura
+Moradia e educação 
+Agora vai para as ruas
+Em busca de solução 
+Pois ainda acreditam
+No fim da escravidão 
+
+E até quem se vendeu
+Na hora da eleição 
+Hoje também reclama
+Daquele político ladrão 
+Pensando em comemorar
+O fim da escravidão 
+
+E a minoria feliz
+Que regem a corrupção 
+Não educam o seu povo
+Por uma simples razão 
+Não querem que aconteça 
+O fim da escravidão
+
+130 anos depois da abolição.`
+  },
+
+  {
+    title: "CANIBAIS",
+    date: "11/09/2025",
+    text: `Ignorando os princípios, 
+Da sua simples existência, 
+Seguindo um trilho,
+Do que acha inteligência,
+Criando o caos,
+Da sua própria decadência.
+
+Destruindo tudo aquilo
+Que acredita que é seu,
+No egocentrismo 
+De quem mesmo se elegeu, 
+Dono de um mundo,
+Do qual nunca mereceu.
+
+Seres capazes, 
+Pensadores, imortais,
+Os mais espertos, 
+Entre todos animais,
+Seres humanos,
+Pobres Seres canibais.
+
+Se espelhando,
+Entre criatura e criador,
+Levando à cruz,
+O seu pai, seu salvador, 
+Criando o início, 
+Do final da sua dor.`
+  },
+
+  {
+    title: "ANJO DA VIDA",
+    date: "09/09/2023",
+    text: `Quando tudo era um fim
+Sem sonho e sem futuro
+Quando tudo amedrontava
+Tudo cinza , tudo escuro
+Os pensamentos sombrios
+E um amanhã inseguro 
+
+Tudo ficou diferente 
+Quando alguém pôde me ouvir
+Escutou o meu silêncio
+E me fez então sorrir
+Quando pode me olhar 
+Me fazendo reagir
+
+Ali fez todo sentido
+Minha batalha vencida
+Todos os tombos tombados
+Todos tropeços da vida
+Vendo a renovação 
+Por uma mão estendida 
+
+Um momento, um instante 
+Sem nenhuma compaixão
+Sem busca, sem interesse
+Sem segunda intenção
+Apenas a delicadeza 
+De saber dar atenção
+
+Um abraço, um carinho
+Simples, sincero, fraterno
+Um olhar de quem não quer
+Ser o céu ou o inferno 
+E que dedica um instante 
+Que se tornará eterno
+
+Uma vida transformada
+Novos ares, novo ser
+Uma força redobrada 
+Uma gana de poder
+Uma vontade explodida
+De seguir e de viver
+
+Um sorriso escancarado
+Espontâneo e com leveza
+Que transforma toda dúvida
+Em uma única certeza
+Somos a mais bela cria
+De toda mãe natureza`
   }
 ];
+
 
 // ====== LÓGICA DO SITE ======
 
